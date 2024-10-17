b7f3167d854fd041174eeca175de667023264b00
Anjarul47 <skanjarulhossain347@gmail.com>
2024-10-18 00:08:29 +0530

    Adding element to the starting

---
+++ LinkedList/AddToStart.java
@@ -0,0 +1,53 @@
+package LinkedList;
+
+//Adding element at the starting of the list
+
+public class AddToStart {
+    class Node{
+        int Data;
+        Node Next;
+        Node(int Data){
+            this.Data=Data;
+            this.Next=null;
+        }
+
+    }
+    Node head;
+
+    public void addList(int Data){
+        Node newNode=new Node(Data);
+        if(head==null){
+            head=newNode;
+            return;
+        }
+        newNode.Next=head;
+        head=newNode;
+
+    }
+
+
+    public void printList(){
+        if(head==null){
+            System.out.println("The List is empty.");
+            return;
+        }
+        Node currNode=head;
+        while(currNode != null){
+            System.out.print(currNode.Data + "--> ");
+            currNode=currNode.Next;
+        }
+    }
+
+    public static void main(String[] args) {
+        AddToStart List=new AddToStart();
+       
+        List.addList(10);
+        List.addList(20);
+        List.addList(30);
+        List.addList(40);
+        List.addList(50);
+        List.addList(60);
+      
+        List.printList();
+    }
+}
